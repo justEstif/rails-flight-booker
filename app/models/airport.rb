7373193c4d2948dc5170f55265be7bc9087166a6
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-18 11:01:59 -0400

    feat: (check desc)
    Established associations for accessing departure and arrival airports from Flight objects, and for retrieving departing and arriving flights from Airport objects

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,6 +1,9 @@
 class Airport < ApplicationRecord
   before_validation :upcase_airport_code, on: [:create, :update]
 
+  has_many :departing_flights, class_name: "Flight", foreign_key: "departure_airport_id"
+  has_many :arriving_flights, class_name: "Flight", foreign_key: "arrival_airport_id"
+
   validates :code, presence: true, uniqueness: true
 
   private

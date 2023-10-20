78146ab74d0abb8502779fa842967d31a072dbd8
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-20 06:34:12 -0400

    feat: pass number of passengers back

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,11 +1,12 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airport_options = [['Select an airport',
+    @departure_airport_options = [["Select an airport",
                                    nil]] + Airport.joins(:departing_flights).distinct.pluck(:code, :id)
-    @arrival_airport_options = [['Select an airport',
+    @arrival_airport_options = [["Select an airport",
                                  nil]] + Airport.joins(:arriving_flights).distinct.pluck(:code, :id)
-    @date_options = [['Select a date', nil]] + Flight.distinct.pluck(:start_time, :start_time)
+    @date_options = [["Select a date", nil]] + Flight.distinct.pluck(:start_time, :start_time)
     @flights = Flight.search(search_params)
+    @number_of_tickets = search_params[:number_of_tickets_select] or 1
   end
 
   private

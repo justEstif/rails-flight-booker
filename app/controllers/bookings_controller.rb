78081e4c82048adb99e59f2aec097825d60444ae
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-21 13:46:53 -0400

    feat: read the number of tickets and selected flight from params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,15 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new(number_of_tickets: new_booking_params[:number_of_tickets], flight_id: new_booking_params[:selected_flight])
+  end
+
+  def create
+    @booking = Booking.new()
+  end
+
+  private
+
+  def new_booking_params
+    params.permit(:number_of_tickets, :selected_flight)
+  end
 end

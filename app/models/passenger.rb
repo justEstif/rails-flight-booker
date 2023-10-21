c0955da6f42a978e829eb88e8152091524aedcc6
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-21 12:57:45 -0400

    feat: passenger model test + fixed fixture

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,21 @@ class Passenger < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   has_many :bookings
   has_many :flights, through: :bookings
+
+  before_validation :lowercase_email, on: %i[create update]
+  before_validation :titlecase_name, on: %i[create update]
+
+  private
+
+  def titlecase_name
+    return if name.nil? || !name.is_a?(String)
+
+    self.name = name.titlecase
+  end
+
+  def lowercase_email
+    return if email.nil? || !email.is_a?(String)
+
+    self.email = email.downcase
+  end
 end
--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -5,16 +5,25 @@ class PassengerTest < ActiveSupport::TestCase
     passenger = Passenger.new(name: "name", email: "test@test.com")
     assert passenger.valid?, "Passenger should be valid with all values present"
   end
-  # test "passenger is invalid when name isn't present" do
-  #   passenger = Passenger.new(email: 'test@test.com')
-  #   assert_not passenger.valid?, 'Flight should not be valid with missing values'
-  # end
-  # test "passenger is invalid when email isn't present" do
-  #   passenger = Passenger.new(name: 'test')
-  #   assert_not passenger.valid?, 'Flight should not be valid with missing values'
-  # end
-  # requires name and email to be created
-  # must be a unique email
-  # stores name in proper case
-  # stores email in lowercase
+  test "passenger is invalid when name isn't present" do
+    passenger = Passenger.new(email: "test@test.com")
+    assert_not passenger.valid?, "passenger should both email and name"
+  end
+  test "passenger is invalid when email isn't present" do
+    passenger = Passenger.new(name: "test")
+    assert_not passenger.valid?, "passenger should both email and name"
+  end
+  test "passenger requires unique email to be used" do
+    Passenger.create(name: "name", email: "test@test.com")
+    passenger = Passenger.new(name: "name", email: "test@test.com")
+    assert_not passenger.valid?, "passenger should have a unique email"
+  end
+  test "stores passenger email in lowercase" do
+    passenger = Passenger.create(name: "name", email: "TEST@TEST.COM")
+    assert_equal "test@test.com", passenger.email, "passenger email should be saved in lowercase"
+  end
+  test "stores passenger name in titlecase" do
+    passenger = Passenger.create(name: "john doe", email: "TEST@TEST.COM")
+    assert_equal "John Doe", passenger.name, "passenger name should be saved in titlecase"
+  end
 end

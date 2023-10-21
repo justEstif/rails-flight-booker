aaa8ed1ae472730382d5125c9fba34fed34951d3
Estifanos <95101485+justEstif@users.noreply.github.com>
2023-10-21 14:22:08 -0400

    feat: new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # Clear existing data
+Booking.destroy_all
 Airport.destroy_all
 Flight.destroy_all
+Passenger.destroy_all
 
 # Create 10 sample airports
 airport_codes = ["JFK", "LAX", "ORD", "ATL", "DFW", "SFO", "MIA", "DEN", "PHX", "LAS"]
@@ -15,7 +17,29 @@ airports = airport_codes.map { |code| Airport.create(code: code) }
     start_time: DateTime.current + rand(365).days,
     end_time: DateTime.current + rand(365).days + rand(24).hours,
     duration: rand(24).hours.to_i,
-    arrival_airport_id: arrival_airport.id,
-    departure_airport_id: departure_airport.id,
+    arrival_airport: arrival_airport,
+    departure_airport: departure_airport
   )
 end
+
+# Create sample passengers
+10.times do |i|
+  Passenger.create(
+    name: "Passenger #{i + 1}",
+    email: "passenger#{i + 1}@example.com"
+  )
+end
+
+# Create sample bookings
+50.times do
+  passenger = Passenger.all.sample
+  flight = Flight.all.sample
+  number_of_tickets = rand(1..5) # Random number of tickets between 1 and 5
+
+  Booking.create(
+    passenger: passenger,
+    flight: flight,
+    number_of_tickets: number_of_tickets
+  )
+end
+
